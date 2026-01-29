1bb12fe35c4b36bed21c9b47e26cb20cfe79c7e7
Junaid Abbas <jjbhatti700@gmail.com>
2026-01-29 17:11:52 +0500

    Fix CSS build

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    cssCodeSplit: false,
+  },
+  base: '/',
 })
